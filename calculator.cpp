fb04a12e61c77a65e43a18e994697691ceeba6bd
hdp12 <helendparker@gmail.com>
2013-11-22 14:29:41 -0500

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -1,68 +1,63 @@
 The following program should function as a basic calculator; it should ask the user to input what type of arithmetic operation he would like, and then ask for the numbers on which the operation should be performed. The calculator should then give the output of the operation.
 
-_______ <iostream>
+#include <iostream>
 
 
-___ multiply(int x, int y)
-{
-  ______ x_y;
+int multiply(int x, int y){
+  return x*y;
 }
 
 
-____ divide(int x, int y)
-{
-  _____ x_y;
+int divide(int x, int y){
+  return x/y;
 }
 
-_____ add(int x, int y) 
-{
-  ______x_y;
+int add(int x, int y){
+  return x+y;
 }
 
-______ subtract(int x, int y)
-{
-  _____x_y;
+int subtract(int x, int y){
+  return x-y;
 }
 
 
 using namespace std;
 
-___ _____()
-{
-  ____ op='c';
-  ____ x, y;
-  while(op!='e')
-  {
-  cout__"What operation would you like to perform: add(+), subtract(-), divide(/), 
-  multiply(*), [e]xit?";
-  cin__op;
-  switch(op)
-  {
-    ____ '+':
-    cin__x;
-    cin__y;
-    cout__x__"+"__y__"="__add(x, y)__endl_
-    break;
-    ____ '-'_
-    cin__x;
-    cin__y;
-    cout__x__"-"__y__"="__subtract(x, y)__endl_
-    break;
-    ____ '/':
-    cin__x;
-    cin__y;
-    cout__x__"/"__y__"="__divide(x, y)__endl_
-    break;
-    ____ '*'_
-    cin__x;
-    cin__y;
-    cout__x__"*"__y__"="__multiply(x, y)__endl_
-    break;
-    _____ 'e':
-    ______;
-    ______:
-    cout__"Sorry, try again"__endl;
+int main(){
+  char op='c';
+  int x, 
+      y;
+  while(op!='e'){
+    cout << "What operation would you like to perform: add(+), subtract(-), divide(/), 
+    multiply(*), or [e]xit?" << endl;
+    cin >> op;
+    
+    switch(op){
+      case '+':
+      cin >> x;
+      cin >> y;
+      cout << x <<"+" << y << "=" << add(x, y) << endl;
+      break;
+      case '-':
+      cin >> x;
+      cin >> y;
+      cout << x << "-" << y << "=" << subtract(x, y) << endl;
+      break;
+      case '/':
+      cin >> x;
+      cin >> y;
+      cout << x << "/" << y << "=" << divide(x, y) << endl;
+      break;
+      case '*':
+      cin >> x;
+      cin >> y;
+      cout << x << "*" << y << "=" << multiply(x, y) << endl;
+      break;
+      case 'e':
+      return 0;
+      default:
+      cout << "Sorry, try again" << endl;
     }
   }
-  return _;
-_
+  return 0;
+}

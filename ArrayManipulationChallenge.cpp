9249ff30ceae6ef851aa9dd6e114fd31623b1f60
hdp12 <helendparker@gmail.com>
2013-11-22 14:04:37 -0500

    Update ArrayManipulationChallenge.cpp

--- ArrayManipulationChallenge.cpp
+++ ArrayManipulationChallenge.cpp
@@ -4,15 +4,13 @@
 
 #include <iostream>
       
-      using namespace std;
-      int main()
-{
-  int array[8];
-  for(int x=0; x<8; x++){
-  cin>>______;
-  for(x=0; x<8; x++){
-  cout<<_____;
-  }
-  }
-  return __;
+using namespace std;
+int main(){
+      int array[8];
+      for(int x=0; x<8; x++)
+      cin>>array[x];
+      for(x=0; x<8; x++)
+      cout<<"array[x];
+      
+      return 0;
 }
